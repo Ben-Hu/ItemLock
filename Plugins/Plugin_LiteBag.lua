e76108108f7973d5f60022d8c3f3c4a04e80a9cc
Ben-Hu <ben-hu@users.noreply.github.com>
2023-10-17 20:22:27 -0400

    fix(litebag): update hooks & slot frame id composition

--- Plugins/Plugin_LiteBag.lua
+++ Plugins/Plugin_LiteBag.lua
@@ -2,13 +2,6 @@ local ItemLock = LibStub("AceAddon-3.0"):GetAddon("ItemLock")
 local PluginLiteBag = ItemLock:NewModule("PluginLiteBag")
 
 function PluginLiteBag:Init(repo, config)
-  hooksecurefunc('LiteBagPanel_UpdateBag', function(bag)
-    for i = 1, bag.size do
-      local slotFrame = bag.itemButtons[i]
-      ItemLock:UpdateSlot(bag:GetID(), slotFrame)
-    end
-  end)
-
   LiteBag_RegisterHook('LiteBagItemButton_Update', function(slotFrame)
     ItemLock:UpdateSlot(slotFrame:GetParent():GetID(), slotFrame)
   end)
@@ -17,5 +10,5 @@ end
 function PluginLiteBag:GetSlotFrame(bagID, slotIndex)
   local containerFrameIndex = bagID + 1
   local itemIndex = slotIndex
-  return _G["LiteBagInventoryPanelContainerFrame" .. containerFrameIndex .. "Item" .. itemIndex]
+  return _G["LiteBagBackpackPanelBag" .. containerFrameIndex .. "Item" .. itemIndex]
 end
